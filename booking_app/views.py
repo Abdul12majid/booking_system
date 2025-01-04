55712703713bb388495c90a3f5f4e8bcbeef20d5
Abdul12majid <yisaabdulmajid@gmail.com>
2025-01-04 19:14:21 +0100

    created base.html/navbar.html/index.html

--- booking_app/views.py
+++ booking_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import Booking
+from .utils import generate_qr_code
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("well")
+	context = {
+
+	}
+	return render(request, 'index.html', context)
