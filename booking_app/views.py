b9545f95f806a14efaad8466b12f63c6df4a61a1
Abdul12majid <yisaabdulmajid@gmail.com>
2025-01-04 19:39:15 +0100

    updated qr saved info => added name, date created

--- booking_app/views.py
+++ booking_app/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Booking
 from .utils import generate_qr_code
+import json
 
 # Create your views here.
 
 def index(request):
-    if request.method == 'POST':
-        name = request.POST['name']
+    if request.method == "POST":
+        name = request.POST["name"]
         booking = Booking.objects.create(name=name)
-        qr_code = generate_qr_code(booking.id)
+        qr_data = json.dumps({
+            "id": str(booking.id),
+            "name": booking.name,
+            "date_created": booking.date_created.strftime("%Y-%m-%d %H:%M:%S"),
+        })
+        qr_code = generate_qr_code(qr_data)
         booking.qr_code.save(f"{booking.id}.png", qr_code)
         booking.save()
-        context = {'booking': booking}
-        print("created")
-        return render(request, 'index.html', context)
-    return render(request, 'index.html')
+        return render(request, "booking_details.html", {"booking": booking})
+    return render(request, "index.html")
+
 
 
 def get_booking_details(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id)
     return render(request, 'booking_details.html', {'booking': booking})
+
